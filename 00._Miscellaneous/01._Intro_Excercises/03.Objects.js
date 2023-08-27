93f0967dd710e78f390f53841a8bbcdd2a6995c0
Ella Frost <ded_19@hotmail.com>
2023-08-27 22:39:46 +0200

    done!

--- 00._Miscellaneous/01._Intro_Excercises/03.Objects.js
+++ 00._Miscellaneous/01._Intro_Excercises/03.Objects.js
@@ -7,11 +7,18 @@ const myObj = { message: "Hello, earthling! I bring peace." };
 
 // Log the message 
 
+console.log(myObj.message);
 // --------------------------------------
 // Exercise 2 - Defining an object. 
 
 // Create an object that has your name and age. 
 
+const person = {
+    name: "Ella Frost",
+    age: 30 
+  };
+  
+  console.log(person);
 
 // --------------------------------------
 // Exercise 3 - Add a property 
@@ -20,6 +27,10 @@ const stackOverflow = {};
 
 // make a rule called isAllowed and let the value be true
 
+stackOverflow.isAllowed = true;
+
+console.log(stackOverflow);
+
 // --------------------------------------
 // Exercise 4 - Remove a property 
 
@@ -27,6 +38,9 @@ const thisSong = { description: "The best song in the world." };
 
 // remove the property "description" and add a property called "about" that should say "Just a tribute." 
 
+delete thisSong.description;
+thisSong.about = "Just a tribute.";
 
+console.log(thisSong);
 // --------------------------------------
 
