8eb9013bc1e45cadcddbc503155520329f68e965
Fluttendut <ded_19@hotmail.com>
2023-09-15 00:11:25 +0200

    correction on first server

--- 01._First_Server/app.js
+++ 01._First_Server/app.js
@@ -3,69 +3,71 @@
 //Initiate Express
 //const app = express();
 
-const app = require("express")();
+// const app = require("express")();
 
 //const oterData =123;
 
 //Can be written as {data: "this is the first request handler", otherData: otherData} to use other data
-app.get("/", (req, res) => {
-    res.send({data: "this is the first request handler"});
-});
+// app.get("/", (req, res) => {
+//     res.send({data: "this is the first request handler"});
+// });
 
 // Task: create a dog endpoint that returns woof
-app.get("/dog", (req, res) => {
-    res.send({dog: "Woof", dog2: "Meow"});
-});
+// app.get("/dog", (req, res) => {
+//     res.send({dog: "Woof", dog2: "Meow"});
+// });
 
 //You can define the id by /:id when you use express 
-app.get("/dog/:firstvalue/:someOtherValue", (req, res) => {
-    console.log(req.params);
-    console.log(req.params.someOtherValue);
-    res.send({dog: "Meow"});
+// app.get("/dog/:firstvalue/:someOtherValue", (req, res) => {
+//     console.log(req.params);
+//     console.log(req.params.someOtherValue);
+//     res.send({dog: "Meow"});
 
-});
+// });
 
 
 //http://localhost:8080/wallet/50
-let ballance = 100;
-app.get("/wallet/:withdrawalAmount", (req,res) =>{
-    const withdrawalAmount = req.params.withdrawalAmount;
-    ballance -= withdrawalAmount;
+// let ballance = 100;
+// app.get("/wallet/:withdrawalAmount", (req,res) =>{
+//     const withdrawalAmount = req.params.withdrawalAmount;
+//     ballance -= withdrawalAmount;
 
-    if (ballance < 0) {
-        ballance += withdrawalAmount;
-        res.send({message: "You can't withdraw. No more money left."})
-    } else {
-        res.send({message: `You have withdrawn ${req.params.withdrawalAmount}`});
-    }
-});
+//     if (ballance < 0) {
+//         ballance += withdrawalAmount;
+//         res.send({message: "You can't withdraw. No more money left."})
+//     } else {
+//         res.send({message: `You have withdrawn ${req.params.withdrawalAmount}`});
+//     }
+// });
 
 //Tomcat port
-//80 http
+// 80 http
 //443 https
 //8080 http developer port
 //9090 htto developer port
 
-// app.listen(8080)const express = require("express");
-// const app = express();
+// app.listen(8080)
 
-// app.use(express.json());
+const express = require("express");
+const app = express();
 
-// app.get("/bla", (req, res) => {
-//     res.send({data: "Hello, Anders says bla bla"});
-// });
+app.use(express.json());
+
+app.get("/bla", (req, res) => {
+    res.send({data: "Hello, Anders says bla bla"});
+});
 
 
 
-// app.get("/bla/:name", (req, res) => {
-//     console.log(req.query);
-//     const saysWhat = req.query.saysWhat || "bla";
-//     res.send({data: `Hello, ${req.params.name} says ${saysWhat}`});
-// });
+app.get("/bla/:name", (req, res) => {
+    console.log(req.query);
+    const saysWhat = req.query.saysWhat || "bla";
+    res.send({data: `Hello, ${req.params.name} says ${saysWhat}`});
+});
 
-// app.post("/dogs", (req, res) => {
-//     console.log(req.body);
-//     res.send({data: req.body});
-// });
+app.post("/dogs", (req, res) => {
+    console.log(req.body);
+    res.send({data: req.body});
+});
 
 app.listen(8080);
--- 01._First_Server_Mountain_PT.I/app.js
+++ 01._First_Server_Mountain_PT.I/app.js
@@ -1,23 +1,24 @@
 const app = require("express")();
 
-const mountains = [];
-mountains.push({id: 1, name: "Mount Doom-poodle"});
-mountains.push({id: 2, name: "Mount Thingymajigity "});
-mountains.push({id: 3, name: "Mount Fluffy"});
-mountains.push({id: 4, name: "Mount not-a-mountain"});
+const mountains = [ 
+    { id: 1, name: "Mount Doom-poodle"},
+    { id: 2, name: "Mount Thingymajigity"},
+    { id: 3, name: "Mount Fluffy"}];
 
 
 app.get("/", (req, res) => {
-    res.send({data: "Write /mountains or /mountains/ followed by an ID number between 1-5"});
+    res.send({data: "Write /mountains or /mountains/ followed by an ID number between 1-3"});
 });
 
 app.get("/mountains", (req, res) => {
-    res.send({Mountains: mountains});
+    res.send({data: mountains});
 });
 
-app.get("/mountains/:id", (req,res) => {
-    res.send({Mountains: mountains.find(mountain => mountain.id === parseInt(req.params.id))});
+app.get("/mountains/:id", (req, res) => {
+    res.send({data: mountains.find(mountain => mountain.id === parseInt(req.params.id))});
 });
+
+
 
 
 app.listen(8080);
